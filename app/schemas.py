5685a3ad1d42e3d2e8001b6fff20603e47a79d4f
Your Name <you@example.com>
2025-10-28 22:35:05 -0400

    Make UserOut.created_at a datetime to match DB timestamp

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
+import datetime
+
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
+
 class UserOut(BaseModel):
     id: int
     email: EmailStr
-    created_at: Optional[str]
+    created_at: Optional[datetime.datetime]
+
 
 class Token(BaseModel):
     access_token: str
